0a317ba96616c5b0c0a5bed087898ede222f89a3
Dharsan <dharsan19082002@gmail.com>
2025-10-04 09:55:41 +0530

    added pandas

---
+++ pandas/main.py
@@ -0,0 +1,15 @@
+import pandas
+
+data = pandas.read_csv("pandas/Squirrel_Data.csv")
+
+gray_squirrel_count = len(data[data["Primary Fur Color"] == "Gray"])
+cinnamon_squirrel_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrel_count = len(data[data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrel_count, cinnamon_squirrel_count, black_squirrel_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("pandas/Squirrel_count.csv")
