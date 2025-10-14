4eb99f007d514a6f1ff1d6d2cc998ce2462aee4e
Dharsan <dharsan19082002@gmail.com>
2025-10-14 21:19:01 +0530

    feat: added sendmail in main.py

--- SMTP/main.py
+++ SMTP/main.py
@@ -6,3 +6,9 @@ my_password = ""
 with smtplib.SMTP("smtp.google.com") as connection:
     connection.starttls()
     connection.login(user=my_email, password=my_password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="",
+        msg="Subject:Hello\n\n" \
+        "This is the body of the email"
+    )
