1fb666c915cfcb7f9573ab0e74db55e9066c7833
Dharsan <dharsan19082002@gmail.com>
2025-10-16 21:57:02 +0530

    feat: added comments

--- SMTP/main.py
+++ SMTP/main.py
@@ -4,6 +4,7 @@ import smtplib
 my_email = ""
 my_password = ""
 
+# invoking smtp 
 with smtplib.SMTP("smtp.google.com") as connection:
     connection.starttls()
     connection.login(user=my_email, password=my_password)
