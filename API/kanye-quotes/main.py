26ba7a691f46dea575c7151d02bc67b520d7f4db
Dharsan <dharsan19082002@gmail.com>
2025-10-25 19:47:19 +0530

    feat: modified main.py

--- API/kanye-quotes/main.py
+++ API/kanye-quotes/main.py
@@ -5,11 +5,10 @@ import requests
 def get_quote():
     #Write your code here.
     response = requests.get(url= "https://api.kanye.rest")
+    response.raise_for_status()
     data = response.json()
     quote = data["quote"]
     canvas.itemconfig(quote_text, text=quote)
-
-
 
 
 window = Tk()
