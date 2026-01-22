0898034a1f948f8bc48cb0600637fe3ffea020fd
Dharsan <dharsan19082002@gmail.com>
2026-01-22 23:20:17 +0530

    Fix typo in file opening statement

--- List_Comprehension/dataOverlap.py
+++ List_Comprehension/dataOverlap.py
@@ -1,9 +1,8 @@
-with open("List_Comprehension/file1.txt", "r") as f1:
+fwith open("List_Comprehension/file1.txt", "r") as f1:
     list1 = [int(num) for num in f1.readlines()]
     
 with open("List_Comprehension/file2.txt", "r") as f2:
     list2 = [int(num) for num in f2.readlines()]
 
 result = [num for num in list1 if num in list2]
-
 print(result)
