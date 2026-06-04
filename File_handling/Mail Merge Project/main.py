a20ff4e88e1e04d6cbe1ba53aa060654e4a313a8
Dharsan <dharsan19082002@gmail.com>
2026-06-04 21:08:09 +0530

    Update main.py

--- File_handling/Mail Merge Project/main.py
+++ File_handling/Mail Merge Project/main.py
@@ -9,7 +9,6 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-
 with open("Input/Names/invited_names.txt", "r") as name_file:
     names = name_file.readlines()
 
