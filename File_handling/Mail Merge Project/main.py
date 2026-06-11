b90b202fe760bb33324259d2ab8013383cac40fe
Dharsan <dharsan19082002@gmail.com>
2026-06-11 21:35:35 +0530

    Update main.py

--- File_handling/Mail Merge Project/main.py
+++ File_handling/Mail Merge Project/main.py
@@ -1,7 +1,5 @@
 #TODO: Create a letter using starting_letter.txt
-
 #for each name in invited_names.txt
-
 #Replace the [name] placeholder with the actual name
 
 #Save the letters in the folder "ReadyToSend".
