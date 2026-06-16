0c66a1c151003e073b9979b76757c769b43a6d62
Dharsan <dharsan19082002@gmail.com>
2026-06-16 22:52:26 +0530

    Update filteringEvenNumbers.py

--- List_Comprehension/filteringEvenNumbers.py
+++ List_Comprehension/filteringEvenNumbers.py
@@ -1,4 +1,5 @@
 # Filtering even numbers from a list of strings using list comprehension
+
 list_of_strings = ['9', '0', '32', '8', '2', '8', '64', '29', '42', '99']
 numbers = [int(num) for num in list_of_strings]
 result = [num for num in numbers if num % 2 == 0]
