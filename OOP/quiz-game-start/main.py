a58caddf0563d33dec9874001ece7af7daeab19c
Dharsan <dharsan19082002@gmail.com>
2026-06-19 21:45:11 +0530

    Update main.py

--- OOP/quiz-game-start/main.py
+++ OOP/quiz-game-start/main.py
@@ -9,7 +9,6 @@ for question in question_data:
     question_answer = question["answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
-
 quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
